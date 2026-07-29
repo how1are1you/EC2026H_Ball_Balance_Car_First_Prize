193c1c17925a5d9367239312d1c8d59955faeb4d
how1are1you <3114188569@qq.com>
2026-07-29 10:33:42 +0800

    初始化

---
+++ Control/control.c
@@ -0,0 +1,294 @@
+/***********************************************
+公司：轮趣科技（东莞）有限公司
+品牌：WHEELTEC
+官网：wheeltec.net
+淘宝店铺：shop114407458.taobao.com
+速卖通: https://minibalance.aliexpress.com/store/4455017
+版本：5.7
+修改时间：2021-04-29
+
+
+Brand: WHEELTEC
+Website: wheeltec.net
+Taobao shop: shop114407458.taobao.com
+Aliexpress: https://minibalance.aliexpress.com/store/4455017
+Version: 5.7
+Update：2021-04-29
+
+All rights reserved
+***********************************************/
+#include "control.h"
+
+u8 CCD_count,ELE_count;
+int Sensor_Left,Sensor_Middle,Sensor_Right,Sensor;
+int Servo=1500;
+Encoder OriginalEncoder; 					//编码器原始数据   
+Motor_parameter MotorA,MotorB;				//左右电机相关变量
+float Velocity_KP=400,Velocity_KI=300;	
+int Run_Mode=0;//小车运行模式
+u8 Flag_Stop=1;//小车启动标志位
+void TIMER_0_INST_IRQHandler(void)
+{
+    if(DL_TimerA_getPendingInterrupt(TIMER_0_INST))
+    {
+        if(DL_TIMER_IIDX_ZERO)
+        {
+			
+			Key();
+			LED_Flash(100);
+			Get_Velocity_From_Encoder(Get_Encoder_countA,Get_Encoder_countB);
+			Get_Encoder_countA=Get_Encoder_countB=0;
+			if(Run_Mode==0)
+			{
+				Get_RC();         //Handle the APP remote commands //处理APP遥控命令
+			}else if(Run_Mode==1){
+				//可于此处添加新模式
+			}
+//			//计算左右电机对应的PWM
+			MotorA.Motor_Pwm = Incremental_PI_Left(MotorA.Current_Encoder,MotorA.Target_Encoder);	
+			MotorB.Motor_Pwm = Incremental_PI_Right(MotorB.Current_Encoder,MotorB.Target_Encoder);
+			Limit_Pwm(7500) ;
+			if(!Flag_Stop)
+			{
+				Set_PWM(MotorA.Motor_Pwm,MotorB.Motor_Pwm,Servo);
+			}else Set_PWM(0,0,0);
+		}
+    }
+}
+
+/**************************************************************************
+Function: Get_Velocity_From_Encoder
+Input   : none
+Output  : none
+函数功能：读取编码器和转换成速度
+入口参数: 无 
+返回  值：无
+**************************************************************************/	 	
+void Get_Velocity_From_Encoder(int Encoder1,int Encoder2)
+{
+	
+	//Retrieves the original data of the encoder
+	//获取编码器的原始数据
+	float Encoder_A_pr,Encoder_B_pr; 
+	OriginalEncoder.A=-Encoder1;	
+	OriginalEncoder.B=-Encoder2;	
+	Encoder_A_pr=OriginalEncoder.A; Encoder_B_pr=-OriginalEncoder.B;
+	//编码器原始数据转换为车轮速度，单位m/s
+	MotorA.Current_Encoder= Encoder_A_pr*Frequency*Perimeter/728.0f;  //2倍频*28减速比*13线数
+	MotorB.Current_Encoder= Encoder_B_pr*Frequency*Perimeter/728.0f;   //2*28*13=728
+	
+}
+//运动学逆解，由x和y的速度得到编码器的速度,Vx是m/s,Vz单位是度/s(角度制)
+void Get_Target_Encoder(float Vx,float Vz)
+{
+	float amplitude=3.5f; //Wheel target speed limit //车轮目标速度限幅
+	float R, AngleR;
+		
+	// For Ackerman small car, Vz represents the front wheel steering Angle
+	//对于阿克曼小车Vz代表右前轮转向角度
+	AngleR=Vz;
+	
+	// Front wheel steering Angle limit (front wheel steering Angle controlled by steering engine), unit: rad
+	//前轮转向角度限幅(舵机控制前轮转向角度)，单位：rad
+	AngleR=target_limit_float(AngleR,-0.52f,0.48f); //限幅在左右30°
+	
+	//计算转弯半径
+	R=Axle_spacing/tan(AngleR)-0.5f*Wheel_spacing;
+	
+	//Inverse kinematics //运动学逆解
+	if(AngleR!=0)
+	{
+		
+		MotorA.Target_Encoder = Vx*(R-0.5f*Wheel_spacing)/R;
+		MotorB.Target_Encoder = Vx*(R+0.5f*Wheel_spacing)/R;	
+		//舵机与角度的关系公式，由采集数据组后3次拟合得出
+		//粗略拟合,8组数据
+		//Servo = 1.49e+03f + 1.01e+03f*AngleR + (-3.73e+02f)*pow(AngleR,2) + 4.21e+02f*pow(AngleR,3) + 10 ; //10为偏差值
+		//精细拟合,14组数据
+		Servo = 1.47e+03f + 8.90e+02f*AngleR + (-2.28e+01f)*pow(AngleR,2) + 4.44e+02f*pow(AngleR,3) ;			
+	}
+	else 
+	{
+		MotorA.Target_Encoder = Vx;
+		MotorB.Target_Encoder = Vx;
+		Servo = Servo_Init;
+	}
+	MotorA.Target_Encoder=target_limit_float(MotorA.Target_Encoder,-amplitude,amplitude); 
+	MotorB.Target_Encoder=target_limit_float(MotorB.Target_Encoder,-amplitude,amplitude); 
+}
+
+
+/**************************************************************************
+Function: Absolute value function
+Input   : a：Number to be converted
+Output  : unsigned int
+函数功能：绝对值函数
+入口参数：a：需要计算绝对值的数
+返回  值：无符号整型
+**************************************************************************/
+int myabs(int a)
+{
+	int temp;
+	if(a<0)  temp=-a;
+	else temp=a;
+	return temp;
+}
+
+int Turn_Off(void)
+{
+	u8 temp = 0;
+//	if(Voltage>700&&EN==0)//电压高于7V且使能开关打开
+//	{
+//		temp = 1;
+//	}
+	return temp;			
+}
+/**************************************************************************
+Function: PWM_Limit
+Input   : IN;max;min
+Output  : OUT
+函数功能：限制PWM赋值
+入口参数: IN：输入参数  max：限幅最大值  min：限幅最小值 
+返回  值：限幅后的值
+**************************************************************************/	 	
+float PWM_Limit(float IN,float max,float min)
+{
+	float OUT = IN;
+	if(OUT>max) OUT = max;
+	if(OUT<min) OUT = min;
+	return OUT;
+}
+/**************************************************************************
+函数功能：增量PI控制器
+入口参数：编码器测量值，目标速度
+返回  值：电机PWM
+根据增量式离散PID公式 
+pwm+=Kp[e（k）-e(k-1)]+Ki*e(k)+Kd[e(k)-2e(k-1)+e(k-2)]
+e(k)代表本次偏差 
+e(k-1)代表上一次的偏差  以此类推 
+pwm代表增量输出
+在我们的速度控制闭环系统里面，只使用PI控制
+pwm+=Kp[e（k）-e(k-1)]+Ki*e(k)
+**************************************************************************/
+int Incremental_PI_Left (float Encoder,float Target)
+{ 	
+	 static float Bias,Pwm,Last_bias;
+	 Bias=Target-Encoder;                					//计算偏差
+	 Pwm+=Velocity_KP*(Bias-Last_bias)+Velocity_KI*Bias;   	//增量式PI控制器
+	 if(Pwm>7800)Pwm=7800;
+	 if(Pwm<-7800)Pwm=-7800;
+	 Last_bias=Bias;	                   					//保存上一次偏差 
+	 return Pwm;                         					//增量输出
+}
+
+
+int Incremental_PI_Right (float Encoder,float Target)
+{ 	
+	 static float Bias,Pwm,Last_bias;
+	 Bias=Target-Encoder;                					//计算偏差
+	 Pwm+=Velocity_KP*(Bias-Last_bias)+Velocity_KI*Bias;   	//增量式PI控制器
+	 if(Pwm>7800)Pwm=7800;
+	 if(Pwm<-7800)Pwm=-7800;
+	 Last_bias=Bias;	                   					//保存上一次偏差 
+	 return Pwm;                         					//增量输出
+}
+/**************************************************************************
+Function: Processes the command sent by APP through usart 2
+Input   : none
+Output  : none
+函数功能：对APP通过串口2发送过来的命令进行处理
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Get_RC(void)
+{
+	u8 Flag_Move=1;
+	 switch(Flag_Direction) //Handle direction control commands //处理方向控制命令
+	 { 
+			case 1:      Move_X=+RC_Velocity;  	 Move_Z=0;         break;
+			case 2:      Move_X=+RC_Velocity;  	 Move_Z=-PI/2;   	 break;
+			case 3:      Move_X=0;      				 Move_Z=-PI/2;   	 break;	 
+			case 4:      Move_X=-RC_Velocity;  	 Move_Z=-PI/2;     break;		 
+			case 5:      Move_X=-RC_Velocity;  	 Move_Z=0;         break;	 
+			case 6:      Move_X=-RC_Velocity;  	 Move_Z=+PI/2;     break;	 
+			case 7:      Move_X=0;     	 			 	 Move_Z=+PI/2;     break;
+			case 8:      Move_X=+RC_Velocity; 	 Move_Z=+PI/2;     break; 
+			default:     Move_X=0;               Move_Z=0;         break;
+	 }
+	 if     (Flag_Left ==1)  Move_Z= PI/2; //left rotation  //左自转 
+	 else if(Flag_Right==1)  Move_Z=-PI/2; //right rotation //右自转	
+	if(Car_Mode==Akm_Car)
+	{
+		//Ackermann structure car is converted to the front wheel steering Angle system target value, and kinematics analysis is pearformed
+		//阿克曼结构小车转换为前轮转向角度
+		Move_Z=Move_Z*2/9; 
+	}
+	//Unit conversion, mm/s -> m/s
+  //单位转换，mm/s -> m/s	
+	Move_X=Move_X/1000;       Move_Y=Move_Y/1000;         Move_Z=Move_Z;
+	//Control target value is obtained and kinematics analysis is performed
+	//得到控制目标值，进行运动学分析
+	Get_Target_Encoder(Move_X,Move_Z);
+}
+
+/**************************************************************************
+Function: Press the key to modify the car running state
+Input   : none
+Output  : none
+函数功能：按键修改小车运行状态
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Key(void)
+{
+	u8 tmp,tmp2;
+	tmp=key_scan(200);//click_N_Double(50);
+	if(tmp==1)
+	{
+		Flag_Stop=!Flag_Stop;
+	}		//单击控制小车的启停
+	else if(tmp==2)
+	{
+		Run_Mode++;
+		Run_Mode%=2;
+	}
+}
+/**************************************************************************
+Function: Limiting function
+Input   : Value
+Output  : none
+函数功能：限幅函数
+入口参数：幅值
+返回  值：无
+**************************************************************************/
+float target_limit_float(float insert,float low,float high)
+{
+    if (insert < low)
+        return low;
+    else if (insert > high)
+        return high;
+    else
+        return insert;
+}
+int target_limit_int(int insert,int low,int high)
+{
+    if (insert < low)
+        return low;
+    else if (insert > high)
+        return high;
+    else
+        return insert;
+}
+/**************************************************************************
+Function: Limit PWM value
+Input   : Value
+Output  : none
+函数功能：限制PWM值
+入口参数：幅值
+返回  值：无
+**************************************************************************/
+void Limit_Pwm(int amplitude)
+{
+    MotorA.Motor_Pwm=target_limit_float(MotorA.Motor_Pwm,-amplitude,amplitude);
+    MotorB.Motor_Pwm=target_limit_float(MotorB.Motor_Pwm,-amplitude,amplitude);
+}
---
+++ empty.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2021, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "board.h"
+u8 Car_Mode=Akm_Car;
+int Motor_Left,Motor_Right;                 //电机PWM变量 应是Motor的
+u8 PID_Send;            //延时和调参相关变量
+float RC_Velocity=200,RC_Turn_Velocity,Move_X,Move_Y,Move_Z,PS2_ON_Flag;               //遥控控制的速度
+float Velocity_Left,Velocity_Right; //车轮速度(mm/s)
+u16 test_num,show_cnt;
+float Voltage=0;
+int Servo_Init=1500;
+int main(void)
+{
+    // 系统初始化
+    SYSCFG_DL_init();  // 初始化系统配置
+    // 清除所有外设的中断挂起状态
+    NVIC_ClearPendingIRQ(ENCODERA_INT_IRQN);    // 编码器A中断
+    NVIC_ClearPendingIRQ(ENCODERB_INT_IRQN);    // 编码器B中断
+    NVIC_ClearPendingIRQ(UART_0_INST_INT_IRQN);  // UART0串口中断
+    NVIC_ClearPendingIRQ(UART_1_INST_INT_IRQN);  // UART1串口中断
+    // 使能各外设的中断
+    NVIC_EnableIRQ(ENCODERA_INT_IRQN);    // 开启编码器A中断
+    NVIC_EnableIRQ(ENCODERB_INT_IRQN);    // 开启编码器B中断
+    NVIC_EnableIRQ(UART_0_INST_INT_IRQN); // 开启UART0中断
+    NVIC_EnableIRQ(UART_1_INST_INT_IRQN); // 开启UART1中断
+    // 定时器和ADC相关中断配置
+    NVIC_ClearPendingIRQ(TIMER_0_INST_INT_IRQN);  // 清除定时器0中断挂起
+    NVIC_EnableIRQ(TIMER_0_INST_INT_IRQN);        // 开启定时器0中断
+    NVIC_EnableIRQ(ADC12_VOLTAGE_INST_INT_IRQN);
+    OLED_Init();  // 初始化OLED显示屏
+	//舵机初始化回归中位
+	DL_Timer_setCaptureCompareValue(PWM_1_INST,Servo_Init,GPIO_PWM_1_C0_IDX);
+    // 主循环
+    while (1) 
+    {
+		Voltage = Get_battery_volt();//采样小车当前电压
+        BTBufferHandler();    // 处理蓝牙数据缓冲区
+        oled_show();         //  OLED显示更新
+        APP_Show();          //  APP显示处理
+    }
+}
+
+
+
