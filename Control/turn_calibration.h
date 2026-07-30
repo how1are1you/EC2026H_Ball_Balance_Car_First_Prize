ea9d80e165ccbeca8c0d1d4cc02b0d62d8d52cf4
how1are1you <3114188569@qq.com>
2026-07-30 13:06:53 +0800

    小改

--- Control/turn_calibration.h
+++ Control/turn_calibration.h
@@ -20,7 +20,7 @@ typedef enum
  * Change this default after the on-track 180-degree arc test is satisfactory.
  * The OLED tuning value is kept while powered, but is not written to flash.
  */
-#define TURN_CALIBRATION_RADIUS_DEFAULT_M (0.465f)
+#define TURN_CALIBRATION_RADIUS_DEFAULT_M (0.475f)
 
 extern volatile TurnCalibrationState_t TurnCalibrationState;
 extern volatile uint8_t TurnCalibrationFault;
